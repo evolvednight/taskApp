79c667d0329d6a5dfff7ec7c0ef0c1c64480901e
evolvednight <edwinh13148@gmail.com>
2020-04-02 01:32:33 -0400

    add buy component routing and task wont render if its false

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { SummaryComponent } from './summary/summary.component';
+import { BuyComponent } from './buy/buy.component';
 
 const routes: Routes = [
   {path: '', component: TasksComponent, data: { animation: 'Task'} },
   {path: 'summary', component: SummaryComponent, data: { animation: "Summary"} },
+  {path: 'buy', component: BuyComponent}
 ];
 
 @NgModule({
